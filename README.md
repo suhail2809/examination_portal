1f7c0f87041622d8184f780935ddb6e3628b1b16
suhail2809 <mdsuhail0703@gmail.com>
2026-09-20 17:39:05 +0530

    MILESTONE-0, added login and signup page

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # examination_portal
+
+
+
+
+
+
+
+
+
+
+
--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-from flask import Flask 
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Examination Management Portal"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
