b5150d283dd9059e6a17ab99d3ee8de8285e0ec2
suhail2809 <mdsuhail0703@gmail.com>
2026-09-23 11:34:08 +0530

    added routes

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-
-from flask import Flask
+from flask import Flask, render_template
+from routes.auth import auth
 
 app = Flask(__name__)
 
+app.register_blueprint(auth)
+
+
 @app.route("/")
-def home():
-    return "Examination Management Portal"
+def index():
+    return render_template("index.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
